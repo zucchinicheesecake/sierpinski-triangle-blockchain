915a0cc2e0cd90b4eabb7358e7b53f5132af73ac
zucchinicheesecake <zalgorythm@icloud.com>
2025-05-25 13:17:06 -0400

    Add frontend/next.config.js 1748193426375

---
+++ frontend/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Accept' },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
