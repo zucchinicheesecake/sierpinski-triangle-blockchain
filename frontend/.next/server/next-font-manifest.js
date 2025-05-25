cf892bef9b6f8dee6df82277267c3e4ceb753cc8
zucchinicheesecake <zalgorythm@icloud.com>
2025-05-25 13:15:55 -0400

    Add frontend/.next/server/next-font-manifest.js 1748193355259

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/project/sandbox/user-workspace/frontend/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/a34f9d1faa5f3315-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
